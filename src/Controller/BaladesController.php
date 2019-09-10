0fe8b557fd9ae58e2badddfcadd1a28425807e3c
MartG31 <31081479+MartG31@users.noreply.github.com>
2019-09-10 15:42:11 +0200

    BaladesController + route + vues

--- src/Controller/BaladesController.php
+++ src/Controller/BaladesController.php
@@ -10,10 +10,56 @@ class BaladesController extends AbstractController
     /**
      * @Route("/balades", name="balades")
      */
-    public function index()
-    {
+    public function indexBalades() {
+
+    	// Affichage du calendrier des balades
+    	// Affichage des dernières balades
+
+
+
+
         return $this->render('balades/index.html.twig', [
-            'controller_name' => 'BaladesController',
+
         ]);
     }
+
+    public function viewBalade() {
+
+    	// Affichage du détail des balades (Titre, dates, description, map)
+    	// Accéder à la galerie
+    	// Bouton "S'inscrire"
+
+    	return $this->render('balades/view.html.twig', [
+
+        ]);
+    }
+
+    public function addBalade() {
+
+    	//
+
+    	return $this->render('balades/add.html.twig', [
+
+        ]);
+    }
+
+    public function editBalade() {
+
+    	//
+
+    	return $this->render('balades/edit.html.twig', [
+
+        ]);
+    }
+
+    public function deleteBalade() {
+
+    	//
+
+    	return $this->render('balades/delete.html.twig', [
+
+        ]);
+    }
+
+
 }
