7c907e0cad7b2a2f8ea0d93b12096a9208c890a8
Mathieuwf3 <52195551+Mathieuwf3@users.noreply.github.com>
2019-09-11 16:14:32 +0200

    try connexion

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,11 +7,5 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
-    /**
-     * @Route("/login", name="user_login")
-     */
-    public function login()
-    {
-        return $this->render('security/login.html.twig', []);
-    }
+    
 }
