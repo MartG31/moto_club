7043b760507ad749e7b81022fb420cb0ca92c99d
MartG31 <31081479+MartG31@users.noreply.github.com>
2019-09-11 11:52:28 +0200

    balades + routes

--- src/Controller/BaladesController.php
+++ src/Controller/BaladesController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Utilisateurs;
 use App\Entity\Balades;
 
 use \Respect\Validation\Validator as v;
@@ -42,13 +43,15 @@ class BaladesController extends AbstractController
 
     	$errors = [];
 
+        $em = $this->getDoctrine()->getManager();
+
         if(!empty($_POST)) {
 
             $post = array_map('trim', array_map('strip_tags', $_POST));
 
-            echo '<pre>';
-            var_dump($post);
-            echo '</pre>';
+            // echo '<pre>';
+            // var_dump($post);
+            // echo '</pre>';
 
             if(!v::notEmpty()->length(3,30)->validate($post['titre'])) {
                 $errors[] = 'Le titre doit comporter entre 3 et 30 caractères';
@@ -68,17 +71,19 @@ class BaladesController extends AbstractController
 
             if(count($errors) === 0) {
 
-                $em = $this->getDoctrine()->getManager();
+                $user = $em->getRepository(Utilisateurs::class)->find(3);
 
                 $bal = new Balades();
+                $bal->setUser($user);
+
                 $bal->setTitre($post['titre']);
                 $bal->setContenu($post['contenu']);
                 $bal->setDateDebut(new \DateTime($post['date_debut']));
                 $bal->setDateFin(new \DateTime($post['date_fin']));
-                // $bal->setDatetimeRdv(new \DateTime($post['datetime_rdv']));
-                // $bal->setAdresseRdv($post['adresse_rdv']);
-                // $bal->setCpRdv($post['cp_rdv']);
-                // $bal->setVilleRdv($post['ville_rdv']);
+                $bal->setDatetimeRdv(new \DateTime($post['datetime_rdv']));
+                $bal->setAdresseRdv($post['adresse_rdv']);
+                $bal->setCpRdv($post['cp_rdv']);
+                $bal->setVilleRdv($post['ville_rdv']);
                 // $bal->setFileGps($post['file_gps']);
                 $bal->setDatetimePost(new \DateTime());
                 // $bal->setDatetimeModif(new \DateTime());
@@ -89,9 +94,17 @@ class BaladesController extends AbstractController
                 $success = true;
             }
 
-
-
         }
+
+        $balades = $em->getRepository(Balades::class)->findAll();
+
+        echo '<pre>';
+        print_r($balades[0]->getUser()->getPrenom());
+        echo '</pre>';
+
+
+
+
 
     	return $this->render('balades/add.html.twig', [
             'post' => $post ?? [],
