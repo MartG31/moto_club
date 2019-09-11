8cbdbd83cccac71fa4d04e141a8304eaa0c4b0bb
Mathieuwf3 <52195551+Mathieuwf3@users.noreply.github.com>
2019-09-11 17:04:14 +0200

    aze

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -13,6 +13,94 @@ class UsersController extends AbstractController
     /**
      * @Route("/users", name="users")
      */
+    public function connexionUser()
+    {
+		// Utilisation de la base de données
+		$em = $this->getDoctrine()->getManager();
+		// Nettoyage des données
+    	$errors = [];
+        
+    	if(!empty($_POST)){
+
+			$safe = array_map('trim', array_map('strip_tags', $_POST));
+
+			if(!filter_var($safe['email'], FILTER_VALIDATE_EMAIL)){
+				$errors[] = 'L\'adresse email est invalide';
+			}
+			
+			$emailExist = $this->getDoctrine()->getRepository(Utilisateurs::class)->findBy(['email' => $safe['email']]);
+			if(!empty($emailExist)){
+				$errors[] = 'L\'adresse email existe déjà';
+			}
+
+			if(!v::stringType()->length(8, null)->validate($safe['password'])){
+				$errors[] = 'Le mot de passe doit comporter au moins 8 caractères';
+			}
+
+			if(!v::equals($safe['password'])->validate($safe['confirmpassword'])) {
+				$errors[] = 'Les mots de passe ne correspondent pas';
+			}
+
+			if(!v::stringType()->length(3, null)->validate($safe['lastname'])){
+				$errors[] = 'Le nom doit comporter au moins 3 caractères';
+			}
+
+			if(!v::stringType()->length(3, null)->validate($safe['firstname'])){
+				$errors[] = 'Le prénom doit comporter au moins 3 caractères';
+			}
+
+			if(!v::notEmpty()->date('Y-m-d')->validate($safe['birthday'])){
+				$errors[] = 'La date de naissance est invalide';
+			}
+
+			if(!v::phone($safe['phone'])){
+				$errors[] = 'Le numéro de téléphone est invalide';
+			}
+
+			if(!v::stringType()->length(7, null)->validate($safe['address'])){
+				$errors[] = 'La rue doit comporter au moins 7 caractères';
+			}
+
+			if(!v::postalCode('FR')->validate($safe['postal_code'])){
+				$errors[] = 'Le code postal est invalide';
+			}
+
+			if(!v::stringType()->length(3, null)->validate($safe['city'])){
+				$errors[] = 'La ville doit comporter au moins 3 caractères';
+			}
+
+    		if(count($errors) == 0){
+
+
+    			/* $articlesData me permet d'utiliser les méthodes de la class App\Entity\Articles.php */
+    			$usersData = new Utilisateurs();
+    			$usersData->setEmail($safe['email'])
+							->setPwd(password_hash($safe['password'], PASSWORD_DEFAULT))
+							->setNom($safe['lastname'])
+							->setPrenom($safe['firstname'])
+							->setTelephone($safe['phone'])
+							->setDateNaiss(new \DateTime($safe['birthday']))
+							->setAdresse($safe['address'])
+							->setCp($safe['postal_code'])
+							->setVille($safe['city'])
+    						->setAcces(1)
+                            ->setDatetimeInscription(new \DateTime('now'));
+
+    			// On prépare la requete.
+    			$em->persist($usersData);
+    			// On l'exécute
+    			$em->flush();
+    			$success = true;
+    		}
+    	}
+
+        return $this->render('users/inscription.html.twig', [
+        	'errors'     => $errors,
+        	'donnees_saisies' => $safe ?? [],
+        	'success' => $success ?? false,
+        ]);
+    }
+
     public function inscriptionUser()
     {
 		// Utilisation de la base de données
@@ -100,4 +188,5 @@ class UsersController extends AbstractController
         	'success' => $success ?? false,
         ]);
     }
+
 }
