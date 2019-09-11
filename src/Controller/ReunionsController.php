49345db573d40412720b3ba99a1b4096b9753f10
alice-elix <52195946+alice-elix@users.noreply.github.com>
2019-09-11 12:20:38 +0200

    maj route + reunions

--- src/Controller/ReunionsController.php
+++ src/Controller/ReunionsController.php
@@ -2,8 +2,16 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface; // Connexion a la base de données
+use Doctrine\ORM\Mapping as ORM;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Respect\Validation\Validator as v;
+use Intervention\Image\ImageManagerStatic as Image;
+
+
 
 use App\Entity\Reunions;
 
@@ -26,14 +34,13 @@ class ReunionsController extends AbstractController
         ]);
     }
 
-    public function viewReunion()
+    public function viewReunion($id)
     
     {
     	// Récupération de la liste des réunions
             $entityManager = $this->getDoctrine()->getManager();
             // Permet de chercher les réunions via le repository
-            #là, c'est un find(), et il faut aller chercher l'id.
-            $reuFound = $entityManager->getRepository(Reunions::class)->findAll();
+            $reuFound = $entityManager->getRepository(Reunions::class)->find($id);
 
 
         return $this->render('reunions/viewReu.html.twig', [
@@ -45,20 +52,74 @@ class ReunionsController extends AbstractController
     public function addReunion()
     
     {
+        if(!empty($_POST)){
 
-        return $this->render('reunions/addReu.html.twig', [
-            'controller_name' => 'ReunionController',
-        ]);
+            $safe = array_map('trim', array_map('strip_tags', $_POST));
+
+            //pas de controle d'unicité de la réunion : il s'agira d'une modération manuelle
+            #$entityManager = $this->getDoctrine()->getManager();
+            #$userFound = $entityManager->getRepository(Reunions::class)->findByEmail($safe['email']);
+            //var_dump($userFound);
+            
+           ///////////////////////////////////////// tableau d'erreur
+
+            $errors = [                                                      
+            (!v::notEmpty()->stringType()->length(3, 80)->validate($safe['titre'])) ? 'L\'intitulé de la réunion doit faire entre 3 et 80 caractères' : null,
+            (!v::notEmpty()->stringType()->length(3, 80)->validate($safe['lieu'])) ? 'Le lieu de réunion doit faire entre 3 et 80 caractères' : null,
+            (!v::notEmpty()->stringType()->length(2, 30)->validate($safe['type'])) ? 'Le type de réunion doit faire entre 2 et 30 caractères' : null,
+            (!v::notEmpty()->stringType()->length(3)->validate($safe['contenu'])) ? 'Le contenu doit faire au moins 3 caractères' : null,
+            (!v::notEmpty()->date()->validate($safe['date'])) ? 'La réunion doit être une date valide' : null,
+            
+            ];
+
+            $errors = array_filter($errors);
+
+            if(count($errors) == 0 ){
+                $success = true;
+
+                   /////////////////////////////////////////// ajout bdd /////////////////////////////////////
+                $entityManager = $this->getDoctrine()->getManager();//
+                
+
+                $reunion = new Réunions();
+                $reunion->setTitre($safe['titre'])
+                        ->setLieuReu($safe['lieu'])
+                        ->setTypeReu($safe['type'])
+                        ->setContenu($safe['contenu'])
+                        ->setDatetimeReu($safe['date'])
+                        ->setDatePublication(new \DateTime('now'));
+                
+
+                // tell Doctrine you want to (eventually) save the Product (no queries yet)
+                $entityManager->persist($reunion);
+
+                // actually executes the queries (i.e. the INSERT query)
+                $entityManager->flush();
+            }
+            else {
+                $errorsForm = implode('<br>', $errors);
+            }
+  
+        }
+
+       return $this->render('reunions/addReu.html.twig', [
+           'success'            => $success ?? false,
+           'errors'             => $errorsForm ?? [],
+           'donnees_saisies'    => $safe ?? [],
+           //'maxSizeFile'          => $this->maxSizeFile,
+
+                   
+        ]);     
     }
 
-    public function editReunion()
+    public function editReunion($id)
     
     {
     	// Récupération de la liste des réunions
             $entityManager = $this->getDoctrine()->getManager();
             // Permet de chercher les réunions via le repository
             #là, c'est un find(), et il faut aller chercher l'id.
-            $reuFound = $entityManager->getRepository(Reunions::class)->findAll();
+            $reuFound = $entityManager->getRepository(Reunions::class)->find($id);
 
         return $this->render('reunions/editReu.html.twig', [
             'controller_name' => 'ReunionController',
@@ -69,6 +130,13 @@ class ReunionsController extends AbstractController
     public function delReunion()
     
     {
+        // Récupération de la liste des réunions
+            $entityManager = $this->getDoctrine()->getManager();
+            // Permet de chercher les réunions via le repository
+            $reuFound = $entityManager->getRepository(Reunions::class)->find($id);
+                //suppression de l'article trouvé
+                #$entityManager->remove($reuFound);
+                #$entityManager->flush();
 
         return $this->render('reunions/delReu.html.twig', [
             'controller_name' => 'ReunionController',
