207ba4b7fef9c53f53cb87861af5c5157dc2f9af
Mathieuwf3 <52195551+Mathieuwf3@users.noreply.github.com>
2019-09-13 17:21:37 +0200

    recup mot de passe

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -8,15 +8,15 @@ use App\Entity\Tokens;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use \Respect\Validation\Validator as v;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 class UsersController extends MasterController {
     /**
      * @Route("/users", name="users")
      */
     public function inscriptionUser() {
-		// Utilisation de la base de données
 		$em = $this->getDoctrine()->getManager();
-		// Nettoyage des données
     	$errors = [];
         
     	if(!empty($_POST)){
@@ -83,9 +83,7 @@ class UsersController extends MasterController {
     						->setAcces('membre')
                             ->setDatetimeInscription(new \DateTime('now'));
 
-    			// On prépare la requete.
     			$em->persist($usersData);
-    			// On l'exécute
     			$em->flush();
     			$success = true;
     			header('Refresh: 1; /users/login');
@@ -100,9 +98,7 @@ class UsersController extends MasterController {
     }
 
     public function loginUser() {
-		// Utilisation de la base de données
 		$em = $this->getDoctrine()->getManager();
-		// Nettoyage des données
     	$errors = [];
         
     	if(!empty($_POST)){
@@ -167,9 +163,8 @@ class UsersController extends MasterController {
     }
 
     public function forgotPasswordUser() {
-		// Utilisation de la base de données
+
 		$em = $this->getDoctrine()->getManager();
-		// Nettoyage des données
     	$errors = [];
         
     	if(!empty($_POST)) {
@@ -195,45 +190,41 @@ class UsersController extends MasterController {
     			$em->flush();
     			$tok->getUser()->getEmail();
 
-    			if ($em->flush()) {
-    				$success = true;
-					$mail = new PHPMailer;
-					$mail->SMTPOptions = ['ssl' => ['verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true]];
-					// $mail->SMTPDebug = 3; // mode debug si > 2
-					$mail->CharSet = 'UTF-8'; // charset utf-8
-					$mail->isSMTP(); // connexion directe à un serveur SMTP
-					$mail->isHTML(true); // mail au format HTML
-					$mail->Host = 'smtp.gmail.com'; // serveur SMTP
-					$mail->SMTPAuth = true; // serveur sécurisé
-					$mail->Port = 465; // port utilisé par le serveur
-					$mail->SMTPSecure = 'ssl'; // certificat SSL
-					$mail->Username = 'mathieu.webforce3@gmail.com'; // login
-					$mail->Password = 'AbC123456789'; // mot de passe
-					$mail->AddAddress($safe['email']); // destinataire
-					// $mail->AddAddress('truc.muche@gmail.com'); // autre destinataire
-					// $mail->AddCC('machin@bidule.fr'); // copie carbone
-					// $mail->AddBCC('patron@societe.com'); // copie cachée
-					$mail->SetFrom('mathieu.webforce3@gmail.com', 'GSM3'); // expéditeur
-					$mail->Subject = 'Message de GSM3'; // sujet
-					// le corps du mail au format HTML
-					$mail->Body = '<html>
-									<head>
-										<style>
-											h1{color: grey;}
-											p{color: grey;}
-										</style>
-									</head>
-									<body>
-										<h1>Mot de passe perdu ?</h1>
-										<p>Bonjour, vous avez indiqué que vous avez perdu votre mot de passe, veuillez cliquer sur le lien suivant pour récupérer l\'accès à votre compte.</p>
-										<p><a href="localhost/site_commu/index.php?page=reinit_password&token='.$token.'">Réinitialiser mon mot de passe</a></p>
-										<p>Si vous n\'êtes pas à l\'origine de cette demande, veuillez ignorer cet email. Vous pouvez continuer à utiliser votre mot de passe actuel.</p>
-									</body>
-								</html>';
-					// envoi
-					$mail->Send();
-    			}
-			    
+				$success = true;
+				$mail = new PHPMailer;
+				$mail->SMTPOptions = ['ssl' => ['verify_peer' => false, 'verify_peer_name' => false, 'allow_self_signed' => true]];
+				// $mail->SMTPDebug = 3; // mode debug si > 2
+				$mail->CharSet = 'UTF-8'; // charset utf-8
+				$mail->isSMTP(); // connexion directe à un serveur SMTP
+				$mail->isHTML(true); // mail au format HTML
+				$mail->Host = 'smtp.gmail.com'; // serveur SMTP
+				$mail->SMTPAuth = true; // serveur sécurisé
+				$mail->Port = 465; // port utilisé par le serveur
+				$mail->SMTPSecure = 'ssl'; // certificat SSL
+				$mail->Username = 'mathieu.webforce3@gmail.com'; // login
+				$mail->Password = 'AbC123456789'; // mot de passe
+				$mail->AddAddress($safe['email']); // destinataire
+				// $mail->AddAddress('truc.muche@gmail.com'); // autre destinataire
+				// $mail->AddCC('machin@bidule.fr'); // copie carbone
+				// $mail->AddBCC('patron@societe.com'); // copie cachée
+				$mail->SetFrom('mathieu.webforce3@gmail.com', 'Amicale BMW Moto 38'); // expéditeur
+				$mail->Subject = 'Récupération de Mot de passe - Amicale BMW Moto 38'; // sujet
+				// le corps du mail au format HTML
+				$mail->Body = '<html>
+								<head>
+									<style>
+										h1{color: grey;}
+										p{color: grey;}
+									</style>
+								</head>
+								<body>
+									<h1>Mot de passe perdu ?</h1>
+									<p>Bonjour, vous avez indiqué que vous avez perdu votre mot de passe, veuillez cliquer sur le lien suivant pour récupérer l\'accès à votre compte.</p>
+									<p><a href="http://127.0.0.1:8000/users/reinit-password?token='.$token.'">Réinitialiser mon mot de passe</a></p>
+									<p>Si vous n\'êtes pas à l\'origine de cette demande, veuillez ignorer cet email. Vous pouvez continuer à utiliser votre mot de passe actuel.</p>
+								</body>
+							</html>';
+				$mail->Send();
 			}
 		}
 
@@ -245,6 +236,32 @@ class UsersController extends MasterController {
 	}
 
     public function reinitPasswordUser() {
+
+    	$em = $this->getDoctrine()->getManager();
+    	$errors = [];
+        
+    	if(!empty($_POST)){
+
+			$safe = array_map('trim', array_map('strip_tags', $_POST));
+			$tokenExists = $this->getDoctrine()->getRepository(Tokens::class)->findOneBy(['token' => $_GET['token']]);
+
+			if(!v::stringType()->length(8, null)->validate($safe['password'])){
+				$errors[] = 'Le mot de passe doit comporter au moins 8 caractères';
+			}
+
+			if(!v::equals($safe['password'])->validate($safe['confirmpassword'])) {
+				$errors[] = 'Les mots de passe ne correspondent pas';
+			}
+
+    		if(count($errors) == 0){
+
+    			$tokenExists->getUser()->setPwd(password_hash($safe['password'], PASSWORD_DEFAULT));
+    			$em->persist($tokenExists);
+    			$em->flush();
+    			$success = true;
+    			header('Refresh: 3; /users/login');
+    		}
+    	}
 		
 
 	    return $this->render('users/reinitpassword.html.twig', [
