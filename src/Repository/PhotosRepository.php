16477d6b863914044b4e5cd550b3864706957089
MartinG <mg.martingagne@gmail.com>
2019-09-17 20:45:45 +0200

    push 1 photos config

--- src/Repository/PhotosRepository.php
+++ src/Repository/PhotosRepository.php
@@ -3,6 +3,7 @@
 namespace App\Repository;
 
 use App\Entity\Photos;
+use App\Entity\Balades;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Common\Persistence\ManagerRegistry;
 
@@ -17,6 +18,16 @@ class PhotosRepository extends ServiceEntityRepository
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Photos::class);
+    }
+
+    public function countPhotosByBalade(Balades $balade) {
+
+    	return $this->createQueryBuilder('p')
+    			->select('count(p.id) as count')
+    	        ->andWhere('p.bal = :balade')
+    	        ->setParameter('balade', $balade)
+    	        ->getQuery()
+    	        ->getOneOrNullResult();
     }
 }
 
