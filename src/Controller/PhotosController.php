51d0684f2610c4e1a0dce74a582189b39a5cc758
MartG31 <31081479+MartG31@users.noreply.github.com>
2019-09-18 17:39:54 +0200

    push photo add js

--- src/Controller/PhotosController.php
+++ src/Controller/PhotosController.php
@@ -51,7 +51,6 @@ class PhotosController extends MasterController
         if($this->restrictAccess('adherent')) { return $this->redirectToRoute('accueil'); }
 
     	$this->errors = [];
-        $em = $this->getDoctrine()->getManager();
 
         if(!empty($_POST)) {
             $post = array_map('trim', array_map('strip_tags', $_POST));
@@ -98,8 +97,13 @@ class PhotosController extends MasterController
 
         $em = $this->getDoctrine()->getManager();
         $ph = $em->getRepository(Photos::class)->find($id);
+
+        // Suppression du fichier
+        unlink($this->uploadDir.$ph->getFileName());
+
         $em->remove($ph);
         $em->flush();
+
 
         return $this->redirectToRoute('gestion_photos', [
             'id' => $ph->getBal()->getId(),
@@ -128,45 +132,84 @@ class PhotosController extends MasterController
         $em = $this->getDoctrine()->getManager();
 
         $balade = $em->getRepository(Balades::class)->find($id);
+
+        // AJOUT PHOTO
+
+        $errors = [];
+
+        if(!empty($_POST) && !empty($_FILES)) {
+
+            // echo '<pre class="alert alert-info mb-0">';
+            // print_r($_FILES);
+            // echo '</pre>';
+
+            if($_FILES['photo']['error'] != UPLOAD_ERR_OK) {
+                $errors[] = 'Une erreur est survenue lors de la sélection du fichier';
+            }
+            else {
+
+                // Init MIME Type
+                $fileInfo = finfo_open(FILEINFO_MIME_TYPE);
+                $mimeType = finfo_file($fileInfo, $_FILES['photo']['tmp_name']);
+                finfo_close($fileInfo);
+
+                if(substr($mimeType, 0, 5) != 'image') {
+                    $errors[] = 'Type de fichier invalide. Vous devez sélectionner un fichier de type image';
+                }
+                else {
+                    $img = Image::make($_FILES['photo']['tmp_name']);
+
+                    if($img->filesize() > $this->maxFileSize) {
+                        $errors[] = 'La taille de votre image ne doit pas exéder 3 MB';
+                    }
+                }
+
+                if(count($errors) === 0) {
+
+                    $path = pathinfo($_FILES['photo']['name']);
+                    $dt = new \DateTime();
+                    $fileName = tr::transliterate($dt->format('Ymd-His').'-'.$balade->getTitle()).'.'.$path['extension'];
+
+                    // Enregistrement en bdd
+                    $user = $em->getRepository(Utilisateurs::class)->find($this->session->get('id'));
+                    
+                    $ph = new Photos();
+                    $ph->setBal($balade);
+                    $ph->setUser($user);
+
+                    $ph->setFileName($fileName);
+                    // $ph->setLegende();
+                    $ph->setDatetimePost(new \DateTime());
+
+                    $em->persist($ph);
+                    $em->flush();
+
+                    // Enregistrement du nouveau fichier
+                    $img->save($this->uploadDir.$fileName);
+
+                    $success = true;
+                }
+            }
+        }
+
+        // DATA GESTION PHOTOS
+
         $photos = $em->getRepository(Photos::class)->findBy(['bal' => $balade]);
         
         
         return $this->render('photos/gestion-photos.html.twig', [
+            'maxFileSize' => $this->maxFileSize, 
+            'uploadDir' => $this->uploadDir,  
             'balade' => $balade,
             'photos' => $photos,
-            'uploadDir' => $this->uploadDir,  
+            'errors' => $errors ?? [],
+            'success' => $success ?? false,
         ]);
     }
 
-    // PRIVATE FUNCTIONS
 
-    private function checkUploadedPhoto(&$img) {
 
-        // echo '<pre>';
-        // var_dump($_FILES);
-        // echo '</pre>';
 
-        if($img->filesize() > $this->maxFileSize) {
-            $this->errors[] = 'La taille de votre image ne doit pas exéder 3 MB';
-        }
-        elseif(substr($img->mime(), 0, 5) != 'image') {
-            $this->errors[] = 'Type de fichier invalide. Vous devez sélectionner un fichier de type image';
-        }
 
-        if(count($this->errors) === 0) {
-
-            if(!is_dir($this->uploadDir)){
-                if(!mkdir($this->uploadDir, 0777)){ 
-                    die('[ERREUR] Lors de la création du répertoire '.$this->uploadDir);
-                }
-            }
-
-            $path = pathinfo($_FILES['photo']['name']);
-            $fileName = tr::transliterate(time().'-'.$path['filename']).'.'.$path['extension'];
-
-            return $fileName;
-        }
-        return false;
-    }
 
  }
