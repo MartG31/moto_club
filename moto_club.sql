d7c9742486f659d610c600359c5ef035fcb17c84
Nebuchadnezzar81 <52195822+Nebuchadnezzar81@users.noreply.github.com>
2019-09-20 10:04:09 +0200

    Update moto_club.sql

--- moto_club.sql
+++ moto_club.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le :  jeu. 19 sep. 2019 à 16:01
+-- Généré le :  ven. 20 sep. 2019 à 09:59
 -- Version du serveur :  10.4.6-MariaDB
 -- Version de PHP :  7.3.9
 
@@ -348,10 +348,10 @@ INSERT INTO `utilisateurs` (`id`, `email`, `pwd`, `acces`, `pseudo`, `nom`, `pre
 (2, 'mathieu.webforce3@gmail.com', '$2y$10$SkNdNrph5tizp5Bf320CcOHzWqm1ERgYp1EdKTMyZCS2i9CQT4KAu', 'admin', NULL, 'CAPDEVILLE', 'Mathieu', NULL, '2019-09-03 12:16:35', NULL, '52 rue des Olivers', '81100', 'Castres', '0123456789', '1990-06-29'),
 (3, 'alice@wf3.com', '$2y$10$p4cwJNA6NqXkltE3iPDIveD7.q2wRsvRfXcoD4EcD0jX7RRQMyk/u', 'admin', NULL, 'ALIX', 'Alice', NULL, '2019-09-04 12:20:54', NULL, '126 chemin de la Farigoule', '13080', 'Aix-en-Provence', '0123456789', '1986-09-18'),
 (6, 'alain.proust@free.fr', '$2y$10$xgaT/CU43oPZE07JqS.I0.qi.FDMMozf8MdgUuej6rRJ7uTKA3iTi', 'bureau', NULL, 'PROUST', 'Alain', '20190919-120956-6.jpg', '2019-09-07 12:34:31', '2019-09-08 17:12:57', '98 avenue de la Contamine', '38210', 'Tullins', '0564879564', '1972-03-22'),
-(7, 'joanna.beauchamp@free.fr', '$2y$10$vjqPODnnXjBGG8uaA9Ltq.NoYwtexBOd0CmB9w9ai2Fslw3qzVG6S', 'bureau', '', 'BEAUCHAMP', 'Joanna', '20190919-121035-7.png', '2019-09-13 12:39:07', NULL, '78 rue de Strasbourg', '63000', 'Clermont-Ferrand', '0435763654', '1982-04-22'),
-(8, 'simone.dionne@free.fr', '$2y$10$tc3GKi9oOVNza0jE/x8vbuVZ1jbxSWVPWy0m6oavB0no/N9Y0bNWy', 'adherent', NULL, 'DIONNE', 'Simone', '20190919-121144-8.jpg', '2019-09-13 12:44:01', NULL, '31 chemin Challet', '87000', 'Limoges', '0564896715', '1959-03-21'),
-(9, 'bertrand.labrie@free.fr', '$2y$10$KLo5eQttqFTnGzqwiHXXiudGz05cwDBNecJfJcWwX0KjP.QISL49W', 'bureau', NULL, 'LABRIE', 'Bertrand', NULL, '2019-09-15 12:28:52', NULL, '85 rue du Général Ailleret', '97139', 'Les Abymes', '0587309198', '1981-05-20'),
-(10, 'audrey.souplet@free.fr', '$2y$10$OnzlTTpJjD.TYFQlbZ22Z.rfzYvytWivKhbGpoQzQx/a4l2oHJSoe', 'adherent', NULL, 'SOUPLET', 'Audrey', NULL, '2019-09-15 12:32:08', NULL, '99 rue Marie de Médicis', '34500', 'Béziers', '0468774517', '1970-09-08'),
+(7, 'joanna.beauchamp@free.fr', '$2y$10$vjqPODnnXjBGG8uaA9Ltq.NoYwtexBOd0CmB9w9ai2Fslw3qzVG6S', 'bureau', '', 'BEAUCHAMP', 'Joanna', '20190919-121035-7.png', '2019-09-13 12:39:07', '2019-09-14 12:30:15', '78 rue de Strasbourg', '63000', 'Clermont-Ferrand', '0435763654', '1982-04-22'),
+(8, 'simone.dionne@free.fr', '$2y$10$tc3GKi9oOVNza0jE/x8vbuVZ1jbxSWVPWy0m6oavB0no/N9Y0bNWy', 'adherent', NULL, 'DIONNE', 'Simone', '20190919-121144-8.jpg', '2019-09-13 12:44:01', '2019-09-20 09:30:00', '31 chemin Challet', '87000', 'Limoges', '0564896715', '1959-03-21'),
+(9, 'bertrand.labrie@free.fr', '$2y$10$KLo5eQttqFTnGzqwiHXXiudGz05cwDBNecJfJcWwX0KjP.QISL49W', 'bureau', NULL, 'LABRIE', 'Bertrand', NULL, '2019-09-15 12:28:52', '2019-09-15 18:45:55', '85 rue du Général Ailleret', '97139', 'Les Abymes', '0587309198', '1981-05-20'),
+(10, 'audrey.souplet@free.fr', '$2y$10$OnzlTTpJjD.TYFQlbZ22Z.rfzYvytWivKhbGpoQzQx/a4l2oHJSoe', 'adherent', NULL, 'SOUPLET', 'Audrey', NULL, '2019-09-15 12:32:08', '2019-09-16 15:26:43', '99 rue Marie de Médicis', '34500', 'Béziers', '0468774517', '1970-09-08'),
 (11, 'theodore.auger@free.fr', '$2y$10$YMiBQZxbwmBQWSoQJ5sokuNhX4gjVrOdn6TnM2uHOaDyjntms5uuW', 'membre', NULL, 'AUGER', 'Théodore', NULL, '2019-09-15 12:33:26', NULL, '35 rue St Ferréol', '92360', 'Meudon-la-Forêt', '0174193770', '1961-12-06'),
 (12, 'marion.fredet@superlicorne.fr', '$2y$10$xPO2UEdzUs/TIUQxUMt1WODrLUTLcBLl41HcsyFO2RKjUQrTC9KHu', 'admin', NULL, 'FREDET', 'Marion', '20190919-121117-12.jpg', '2019-09-18 10:31:31', NULL, '23 avenue des licornes (appt 67)', '31697', 'Unicorncity', '0619437619', '1789-04-01'),
 (14, 'simon.cathala@wf3.com', '$2y$10$UEwBNTWjnWVUKkr.i8bWZubkW1TGc9lbECTUUehzLUXj3gaM7AJ76', 'admin', NULL, 'CATHALA', 'Simon', NULL, '2019-09-18 10:39:39', NULL, '666 rue des guillotines', '31444', 'Toulouse', '0637619437', '1985-12-27');
